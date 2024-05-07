0d5fc4f06d4840d09249213f2ab5f84a6fccdcac
Ralph <rmj.ralph@gmail.com>
2024-05-07 11:33:55 +0200

    new commit...

---
+++ OurNewRepository/src/main/java/com/mycompany/ournewrepository/OurNewRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ournewrepository;
+
+/**
+ *
+ * @author HP
+ */
+public class OurNewRepository {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
